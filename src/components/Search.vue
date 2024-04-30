6bd0053e357c24a94c1286d1599f3f8e84243a46
Florian van der Galiën <florian.vandergalien@tmt.de>
2024-04-30 21:26:03 +0200

    Add main layout

---
+++ src/components/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <form>
+    <div class="relative">
+      <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+      <Input
+        type="search"
+        placeholder="Search products..."
+        class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+      />
+    </div>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { Search } from 'lucide-vue-next'
+import { Input } from '@/components/ui/input'
+</script>
---
+++ src/components/UserMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="secondary" size="icon" class="rounded-full">
+        <CircleUser class="h-5 w-5" />
+        <span class="sr-only">Toggle user menu</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>Settings</DropdownMenuItem>
+      <DropdownMenuItem>Support</DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>Logout</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<script setup lang="ts">
+import { CircleUser } from 'lucide-vue-next'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+</script>
