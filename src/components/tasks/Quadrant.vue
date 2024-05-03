0d2b5dafce65e8df5742f128edcf9d7595acb9fd
Florian van der Galiën <florian.vandergalien@tmt.de>
2024-05-03 23:12:33 +0200

    Add single Quadrants vue component
    and implement basic growing cards logic

---
+++ src/components/tasks/Quadrant.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="[`prio-${priority}`, classes]" class="rounded-lg p-4">
+    <input
+      type="text"
+      class="tasks w-full rounded-md border border-gray-300 px-2 py-3 text-black"
+      placeholder="Add a task..."
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  priority: 1 | 2 | 3 | 4
+}>()
+
+const classes = computed(() => ({
+  'bg-lime-100': props.priority === 1,
+  'bg-sky-100': props.priority === 2,
+  'bg-yellow-100': props.priority === 3,
+  'bg-red-100': props.priority === 4,
+}))
+</script>
---
+++ src/components/tasks/Quadrants.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="h-full">
+    <div id="quadrants" class="h-full">
+      <Quadrant
+        :priority="prio"
+        v-for="prio in prios"
+        :key="`task-card-${prio}`"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Quadrant from '@/components/tasks/Quadrant.vue'
+
+const prios = [1, 2, 3, 4] as const
+</script>
+
+<style>
+#quadrants {
+  display: grid;
+  gap: 1.5rem;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+
+  & > div {
+    grid-column: span 2;
+    grid-row: span 2;
+  }
+
+  &:has(:focus) {
+    .prio-1 {
+      &:has(:focus) {
+        grid-column: 1 / -2;
+        grid-row: 1 / -2;
+      }
+
+      &:not(:has(:focus)) {
+        grid-column: 1 / 2;
+        grid-row: 1 / 4;
+      }
+    }
+
+    .prio-2 {
+      &:has(:focus) {
+        grid-column: 2 / -1;
+        grid-row: 1 / -2;
+      }
+
+      &:not(:has(:focus)) {
+        grid-column: -2 / -1;
+        grid-row: 1 / 4;
+      }
+    }
+
+    .prio-3 {
+      &:has(:focus) {
+        grid-column: 1 / -2;
+        grid-row: -1 / 2;
+      }
+
+      &:not(:has(:focus)) {
+        grid-column: 1 / 2;
+        grid-row: 2 / -1;
+      }
+    }
+
+    .prio-4 {
+      &:has(:focus) {
+        grid-column: 2 / -1;
+        grid-row: -1 / 2;
+      }
+
+      &:not(:has(:focus)) {
+        grid-column: -2 / -1;
+        grid-row: 2 / -1;
+      }
+    }
+  }
+}
+</style>
