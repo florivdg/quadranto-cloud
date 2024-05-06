a67f31f33a9e58fb0634ddcfde3a9f88dbb88de0
Florian van der Galiën <hallo@flori.dev>
2024-05-06 21:44:32 +0200

    Add basic quadrant layout and make Main layout component configurable

--- src/components/tasks/Quadrant.vue
+++ src/components/tasks/Quadrant.vue
@@ -1,29 +1,37 @@
 <template>
-  <Card>
-    <CardHeader>
-      <CardTitle>{{ title }}</CardTitle>
+  <Card class="flex flex-col">
+    <CardHeader class="bg-muted/40">
+      <CardTitle class="truncate leading-normal">{{ title }}</CardTitle>
     </CardHeader>
-    <CardContent>
+    <CardContent class="flex-1 overflow-y-auto py-0">
+      <ul>
+        <li v-for="task in tasks">{{ task.title }}</li>
+      </ul>
+      <ul>
+        <li v-for="task in tasks">{{ task.title }}</li>
+      </ul>
+      <ul>
+        <li v-for="task in tasks">{{ task.title }}</li>
+      </ul>
+      <ul>
+        <li v-for="task in tasks">{{ task.title }}</li>
+      </ul>
       <ul>
         <li v-for="task in tasks">{{ task.title }}</li>
       </ul>
     </CardContent>
-    <CardFooter></CardFooter>
   </Card>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import type { Priority, Task } from '@/db/schema'
 
+/**
+ * Props.
+ */
 const props = defineProps<{
   priority: Priority
   tasks: Task[]
--- src/components/tasks/Quadrants.vue
+++ src/components/tasks/Quadrants.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="h-full">
-    <div id="quadrants" class="grid h-full grid-cols-2 grid-rows-2 gap-4">
-      <Quadrant
-        :priority="prio"
-        :tasks="tasksForPriority(prio)"
-        v-for="prio in prios"
-        :key="`task-card-${prio}`"
-      />
+  <div class="h-full overflow-hidden @container/project">
+    <div
+      class="h-full overflow-y-auto p-4 @2xl/project:h-full @2xl/project:overflow-hidden lg:p-6"
+    >
+      <div
+        class="grid grid-cols-1 gap-4 @2xl/project:h-full @2xl/project:grid-cols-2 @2xl/project:grid-rows-2 @2xl/project:overflow-hidden"
+      >
+        <Quadrant
+          :priority="prio"
+          :tasks="tasksForPriority(prio)"
+          v-for="prio in prios"
+          :key="`task-card-${prio}`"
+        />
+      </div>
     </div>
   </div>
 </template>
