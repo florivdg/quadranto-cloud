2b8c34f02302dfb88d3d24f9e12145613cad9011
Florian van der Galiën <hallo@flori.dev>
2024-05-19 06:49:33 +0200

    fix: Update database configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,9 +7,9 @@ config({ path: './.env' })
 export default {
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DB_URL!,
+    url: process.env.DB_URL!,
   },
   verbose: true,
   strict: true,
